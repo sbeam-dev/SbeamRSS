15fdb980b24b60813633e3b58e68757be31491be
LastMC <last_mc@126.com>
2020-01-14 18:20:34 +0800

    Fix import error

--- lib/subs.dart
+++ lib/subs.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app1/models/sourceModel.dart';
+import 'package:flutter_app1/models/sourcemodel.dart';
 import 'package:provider/provider.dart';
 
 import 'sourcedb.dart';
