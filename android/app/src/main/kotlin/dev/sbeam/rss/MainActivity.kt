7ee7ebee8555fd61e7fa8901f8c6071e84b8c7c4
SuperAIdesu <tjx10011@gmail.com>
2020-11-30 19:32:49 +0800

    Bump flutter and SDK version

--- android/app/src/main/kotlin/dev/sbeam/rss/MainActivity.kt
+++ android/app/src/main/kotlin/dev/sbeam/rss/MainActivity.kt
@@ -1,12 +1,10 @@
 package dev.sbeam.rss
 
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+//import android.os.Bundle
+//import io.flutter.app.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+
 }
